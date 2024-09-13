0749e7ea3d58ba107c00c6bb16ec2e713c0f14f3
Mahin Khan <md.mahin.bd18@gmail.com>
2024-09-14 03:33:30 +0600

    Started Booking impl

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const { serverConfig } = require('./config');
 
 const app = express();
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 app.use('/api', apiRoutes);
 
 app.listen(serverConfig.PORT, () => {
--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -1,3 +1,30 @@
+const axios = require('axios');
+const { BookingRepository } = require('../repositories');
+const db = require('../models');
+const {serverConfig} = require('../config');
+const AppError = require('../utils/errors/app-error');
+const { StatusCodes } = require('http-status-codes');
+
+
+async function createBooking(data) {
+    return new Promise(async (resolve, reject) => {
+        
+            const result = db.sequelize.transaction(async function bookingImp(t) {
+                const flight = await axios.get(`${serverConfig.FLIGHT_SERVICE}/api/v1/flights/${data.flightId}`);
+                const flightData = flight.data.data;
+                if (data.noOfSeats > flightData.totalSeats) {
+                    reject(new AppError('Not Enough seats avaiable', StatusCodes.BAD_REQUEST));
+                }
+                resolve(true);
+            });
+
+        
+          
+        
+     })
+
+}
+
 module.exports = {
-    
+    createBooking
 }
