6635a8728bec7643ebe30196e3fd2314a4aa4bd0
Mahin Khan <md.mahin.bd18@gmail.com>
2024-09-17 17:50:56 +0600

    made proxy compliant

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api', apiRoutes);
+app.use('/bookingService/api',apiRoutes)
 
 app.listen(serverConfig.PORT, async() => {
     console.log(`Successfully started on the server on PORT : ${serverConfig.PORT}`);
